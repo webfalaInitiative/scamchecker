d8103fff58eb2e9a99ff92f9d17fe1d5da21ebfe
khodijat lawal <khodijahlawal@gmail.com>
2025-05-22 08:36:40 -0700

    feedback

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="md:py-6 py-3">
         <div className="items-center justify-center h-fit flex flex-col p-6">
           <h1 className="md:text-6xl text-3xl md:w-[1000px] w-full font-bold text-secondary text-center">
-            Stay Safe Online with AI – Instantly Detect Phishing Links
+            Stay Safe Online with AI – Instantly Detect Phishing Link.
           </h1>
           <p className="text-[#424242] w-full  md:flex md:w-[800px] mt-3 items-center justify-center">
             Protect Yourself from Cyber Threats with AI-Powered Security.
